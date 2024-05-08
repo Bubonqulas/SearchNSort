bc0d6cd11ca647c910a0be6630ab10a474685dd6
Bubonqulas <hassandarky@Gmail.com>
2024-05-08 13:29:07 -0600

    SearchNSort

---
+++ SearchNSort.java
@@ -0,0 +1,14 @@
+public class SearchNSort {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to your Board Game Directory!\n-----");
+        choiceMenu();
+
+    }
+
+    public static void choiceMenu() {
+
+        System.out.println(
+                "\nWhat would you like to do?\n-----\n1. Add a Game\n2. Print Games Alphabetically\n3. Print Games Based on Difficulty\n4. Print Games Based on Genre\n5. Print Games Based on Time\n6. Save and Exit");
+    }
+}
