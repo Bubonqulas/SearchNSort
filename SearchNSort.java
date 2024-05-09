9d5deaeb50789d0145052694b04b1b14e11d2641
Bubonqulas <hassandarky@Gmail.com>
2024-05-09 13:00:17 -0600

    yes

--- SearchNSort.java
+++ SearchNSort.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SearchNSort {
 
     public static void main(String[] args) {
@@ -8,7 +10,25 @@ public class SearchNSort {
 
     public static void choiceMenu() {
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(
-                "\nWhat would you like to do?\n-----\n1. Add a Game\n2. Print Games Alphabetically\n3. Print Games Based on Difficulty\n4. Print Games Based on Genre\n5. Print Games Based on Time\n6. Save and Exit");
-    }
+         "\nWhat would you like to do?\n-----\n1. Add a Game\n2. Print Games Alphabetically\n3. Print Games Based on Difficulty\n4. Print Games Based on Genre\n5. Print Games Based on Time\n6. Save and Exit");
+         int input = scanner.nextInt();
+
+         if(input == 1){
+
+        } else if(input == 2){
+
+        }else if(input == 3){
+
+        }else if(input == 4){
+
+        }else if(input == 5){
+            
+        }
+
+
+
+
 }
